57c0c2b3a231dcf8065adf8a8235a8255fffccd1
有理想的鸭子 <578541570@qq.com>
2019-10-30 20:55:49 +0800

    清空选择的内容

--- static/js/components/agent_select.js
+++ static/js/components/agent_select.js
@@ -2,6 +2,13 @@ define({
     props:{
       value:{required:true}
     },
+    watch: {
+      value(newVal, oldVal){
+        if(!newVal){
+          this.labelValue = '';
+        }
+      }
+    },
     data: function () {
       return {
         labelValue: '',
@@ -38,9 +45,9 @@ define({
       </el-input>
       <div>
         <my-table url="/agent/list" ref="mainTable" :selectable="false" @done-load="loadDong">
-          <el-table-column prop="id" label="ID"/>
+          <el-table-column prop="id" label="ID" width="50"/>
           <el-table-column prop="sn" label="sn" width="110"/>
-          <el-table-column prop="name" label="名称"/>
+          <el-table-column prop="name" label="名称" width="150"/>
           <el-table-column label="操作">
             <template slot-scope="scope">
               <el-button @click="select(scope.row)" type="text" size="small">选择</el-button>
