b5f077ec27ee2c99df93ec4144513896e30a155c
gdouyang <578541570@qq.com>
2022-10-05 20:39:50 +0800

    http server

--- network/servers/http/http_server.go
+++ network/servers/http/http_server.go
@@ -2,14 +2,35 @@ package httpserver
 
 import (
 	"fmt"
+	"go-iot/codec"
 	"net/http"
+
+	"github.com/beego/beego/v2/core/logs"
 )
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+func ServerStart(network codec.Network) {
+	spec := &HttpServerSpec{}
+	spec.FromJson(network.Configuration)
+	spec.Port = network.Port
+
+	if len(spec.Paths) == 0 {
+		spec.Paths = append(spec.Paths, "/")
+	}
+
+	for _, path := range spec.Paths {
+		http.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+			socketHandler(w, r, network.ProductId)
+		})
+	}
+	addr := spec.Host + ":" + fmt.Sprint(spec.Port)
+
+	err := http.ListenAndServe(addr, nil)
+
+	if err != nil {
+		logs.Error(err)
+	}
 }
 
-func ServerStart() {
-	http.HandleFunc("/", indexHandler)
-	http.ListenAndServe(":8000", nil)
+func socketHandler(w http.ResponseWriter, r *http.Request, productId string) {
+	fmt.Fprintf(w, "hello world "+productId)
 }
