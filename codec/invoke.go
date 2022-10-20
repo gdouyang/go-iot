f9dbb680c0336905cc656c9c9b8255ea34ad6f23
gdouyang <578541570@qq.com>
2022-10-20 22:16:37 +0800

    invoke

--- codec/invoke.go
+++ codec/invoke.go
@@ -1,19 +1,43 @@
 package codec
 
 import (
+	"context"
 	"errors"
 	"go-iot/codec/msg"
+	"time"
 )
 
 // 进行功能调用
 func DoCmdInvoke(productId string, message msg.FuncInvoke) error {
 	session := sessionManager.Get(message.DeviceId)
 	if session == nil {
-		return errors.New("设备不在线")
+		return errors.New("device is offline")
 	}
 	codec := GetCodec(productId)
-	return codec.OnInvoke(&FuncInvokeContext{session: session,
-		deviceId: message.DeviceId, productId: productId})
+	if codec == nil {
+		return errors.New("not found codec")
+	}
+	// timeout of invoke
+	ctx, cancel := context.WithTimeout(context.Background(), (time.Second * 10))
+	defer cancel()
+
+	result := make(chan error)
+	go func(ctx context.Context) {
+		err := codec.OnInvoke(&FuncInvokeContext{
+			deviceId:  message.DeviceId,
+			productId: productId,
+			session:   session,
+			message:   message,
+		})
+		result <- err
+	}(ctx)
+
+	select {
+	case <-ctx.Done():
+		return errors.New("timeout")
+	case err := <-result:
+		return err
+	}
 }
 
 // 功能调用
@@ -22,6 +46,7 @@ type FuncInvokeContext struct {
 	session   Session
 	deviceId  string
 	productId string
+	// functionId string
 }
 
 func (ctx *FuncInvokeContext) GetMessage() interface{} {
