59202da67b983bb917d6320ead7adaf7632a1dd5
gdouyang <578541570@qq.com>
2022-11-14 20:33:04 +0800

    event bus

--- codec/eventbus/bus.go
+++ codec/eventbus/bus.go
@@ -9,30 +9,41 @@ const (
 	DeviceMessageTopic string = "/device/%s/%s/property"
 )
 
-var b = &eventBus{
-	m: map[string][]func(data interface{}){},
+var b = newEventBus()
+
+func newEventBus() *eventBus {
+	return &eventBus{
+		m:       map[string][]func(data interface{}){},
+		matcher: *NewAntPathMatcher(),
+	}
 }
 
 type eventBus struct {
 	sync.Mutex
-	m map[string][]func(data interface{})
+	m       map[string][]func(data interface{})
+	matcher AntPathMatcher
 }
 
-func Subscribe(topic string, run func(data interface{})) {
+func (b *eventBus) match(pattern string, path string) bool {
+	return b.matcher.Match(pattern, path)
+}
+
+func Subscribe(pattern string, run func(data interface{})) {
 	b.Lock()
 	defer b.Unlock()
-	if _, ok := b.m[topic]; ok {
-		b.m[topic] = append(b.m[topic], run)
+	if _, ok := b.m[pattern]; ok {
+		b.m[pattern] = append(b.m[pattern], run)
 	}
 }
 
 func Publish(topic string, data interface{}) {
 	b.Lock()
 	defer b.Unlock()
-	if _, ok := b.m[topic]; ok {
-		listener := b.m[topic]
-		for _, callback := range listener {
-			callback(data)
+	for pattern, listener := range b.m {
+		if b.match(pattern, topic) {
+			for _, callback := range listener {
+				callback(data)
+			}
 		}
 	}
 }
