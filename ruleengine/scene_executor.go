359ca1bf0a31f6a5dc1a061a6069f69da8237e91
gdouyang <578541570@qq.com>
2022-11-15 20:54:53 +0800

    scene debug

--- ruleengine/scene_executor.go
+++ ruleengine/scene_executor.go
@@ -1,9 +1,9 @@
 package ruleengine
 
 import (
-	"encoding/json"
-	"fmt"
 	"go-iot/codec/eventbus"
+
+	"github.com/beego/beego/v2/core/logs"
 )
 
 type SceneExecutor struct {
@@ -12,28 +12,32 @@ type SceneExecutor struct {
 }
 
 func (s *SceneExecutor) Init() {
-	eventbus.Subscribe(fmt.Sprintf(eventbus.DeviceMessageTopic, "*", "*"), func(data interface{}) {
-		s.doStart(data)
-	})
-}
-
-func (s *SceneExecutor) doStart(data interface{}) {
-	var pass bool
 	for _, trigger := range s.Triggers {
-		device := trigger.Device
-		for _, filter := range device.Filters {
-			pass, _ = filter.evaluate(data.(map[string]interface{}))
-			goto here
+		if trigger.Type == TriggerTypeDevice {
+			device := trigger.Device
+			eventbus.Subscribe(eventbus.GetDeviceMesssageTopic(device.ProductId, device.DeviceId), func(data interface{}) {
+				s.doStart(device, data)
+			})
 		}
 	}
-here:
+}
+
+func (s *SceneExecutor) doStart(device SceneTriggerDevice, data interface{}) {
+	var pass bool
+	for _, filter := range device.Filters {
+		pass, _ = filter.evaluate(data.(map[string]interface{}))
+		break
+	}
 	if pass {
 		for _, action := range s.Actions {
 			if action.Executor == "device-message-sender" {
-				a := &DeviceCmdAction{}
-				b, _ := json.Marshal(action.Configuration)
-				a.FromJson(string(b))
-				a.Do()
+				a := DeviceCmdAction{}
+				err := a.Covent(action.Configuration)
+				if err != nil {
+					logs.Error(err)
+				} else {
+					a.Do()
+				}
 			}
 		}
 	}
