4d7d33a760c5b946dfe1771a32e39907106530af
gdouyang <578541570@qq.com>
2022-11-21 21:21:57 +0800

    triggert test

--- ruleengine/spec_test.go
+++ ruleengine/spec_test.go
@@ -51,4 +51,16 @@ func TestTirgger(t *testing.T) {
 		},
 	}
 	assert.Equal(t, "age == 1 && age == 2 && name == 3 || (age == 2) || (age == 3)", trigger.GetExpression())
+
+	trigger = ruleengine.Trigger{
+		FilterType: "event",
+		Filters: []ruleengine.ConditionFilter{
+			{Logic: "and", Key: "age", Operator: "eq", Value: "1"},
+			{Logic: "and", Key: "age", Operator: "eq", Value: "2"},
+			{Logic: "or", Key: "age", Operator: "eq", Value: "2"},
+			{Logic: "or", Key: "age", Operator: "eq", Value: "3"},
+			{Logic: "and", Key: "name", Operator: "eq", Value: "3"},
+		},
+	}
+	assert.Equal(t, "age == 1 && age == 2 || (age == 2) || (age == 3 && name == 3)", trigger.GetExpression())
 }
