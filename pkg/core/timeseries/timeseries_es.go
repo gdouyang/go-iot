eb666fb8419af2d353227794022d570616f38944
有理想的鸭子 <578541570@qq.com>
2023-08-06 17:28:57 +0800

    长度修改

--- pkg/core/timeseries/timeseries_es.go
+++ pkg/core/timeseries/timeseries_es.go
@@ -348,7 +348,7 @@ func (t *EsTimeSeries) createElasticProperty(p tsl.TslProperty) any {
 	case tsl.TypeEnum:
 		return es.Property{Type: "keyword", IgnoreAbove: "256"}
 	case tsl.TypeString:
-		return es.Property{Type: "keyword", IgnoreAbove: "256"}
+		return es.Property{Type: "keyword"}
 	case tsl.TypePassword:
 		return es.Property{Type: "keyword", IgnoreAbove: "256"}
 	case tsl.TypeDate:
