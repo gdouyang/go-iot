c56c29c47f3e188f0c40b942571c4e163bbd61ea
有理想的鸭子 <578541570@qq.com>
2023-08-06 11:10:07 +0800

    添加注释

--- pkg/network/servers/websocket/session.go
+++ pkg/network/servers/websocket/session.go
@@ -97,6 +97,11 @@ func (s *WebsocketSession) SendBinary(msg string) error {
 	return nil
 }
 
+// readLoop pumps messages from the websocket connection to the hub.
+//
+// The application runs readPump in a per-connection goroutine. The application
+// ensures that there is at most one reader on a connection by executing all
+// reads from this goroutine.
 func (s *WebsocketSession) readLoop() {
 	defer func() {
 		s.Disconnect()
